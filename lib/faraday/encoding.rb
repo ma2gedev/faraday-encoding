bea825c5b7e9f1519b93ff58c8d07cd5fc9bc98b
Myk <mkyryk@payrollhero.com>
2016-02-17 09:37:38 +0800

    Fix an issue with a non standard encoding name (explicitly: utf8).
    This makes it possible to add mappings for a non standard encoding names.
    Ex: utf8 should be converted to a proper name "utf-8".
    Adds spec to check that mapping is working and that we do not enforce encoding if we can't find one.

--- lib/faraday/encoding.rb
+++ lib/faraday/encoding.rb
@@ -2,17 +2,47 @@ require "faraday"
 
 module Faraday
   class Faraday::Encoding < Faraday::Middleware
+    def self.mappings
+      {
+        'utf8' => 'utf-8'
+      }
+    end
+
     def call(environment)
       @app.call(environment).on_complete do |env|
-        if /charset=([^;|$]+)/.match(env[:response_headers][:content_type])
-          begin
-            content_charset = ::Encoding.find Regexp.last_match(1)
-          rescue
-            nil
-          end
-        end
-        env[:body].force_encoding content_charset if content_charset
+        @env = env
+        env[:body].force_encoding(content_charset) if content_charset
       end
+    end
+
+    private
+
+    # @return [Encoding|NilClass] returns Encoding or nil
+    def content_charset
+      @content_charset ||= ::Encoding.find encoding_name rescue nil
+    end
+
+    # @return [String] returns a string representing encoding name if it is find in the CONTENT TYPE header
+    def encoding_name
+      @encoding_name ||= begin
+        if /charset=([^;|$]+)/.match(content_type)
+          mapped_encoding(Regexp.last_match(1))
+        end
+      end
+    end
+
+    # @param [String] encoding_name
+    # @return [String] tries to find a mapping for the encoding name
+    # ex: returns 'utf-8' for encoding_name 'utf8'
+    # if mapping is not found - return the same input parameter `encoding_name`
+    # Look at `self.mappings` to see which mappings are available
+    def mapped_encoding(encoding_name)
+      self.class.mappings.fetch(encoding_name, default = encoding_name)
+    end
+
+    # @return [String]
+    def content_type
+      @env[:response_headers][:content_type]
     end
   end
 end
