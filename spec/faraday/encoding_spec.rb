b6b72cd27ed22a4d12f63ae20ed865eeea2a5548
Juan Schwindt <juan@schwindt.org>
2016-03-24 17:21:22 -0300

    Add specs for redirect with encoding

--- spec/faraday/encoding_spec.rb
+++ spec/faraday/encoding_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Faraday::Encoding do
   let(:client) do
     Faraday.new do |connection|
+      connection.use FaradayMiddleware::FollowRedirects, limit: 10
       connection.response :encoding
       connection.adapter :test, stubs
     end
@@ -12,6 +13,9 @@ describe Faraday::Encoding do
     Faraday::Adapter::Test::Stubs.new do |stub|
       stub.get('/') do
         [response_status, response_headers, response_body]
+      end
+      stub.get('/redirected') do
+        [redirected_status, redirected_headers, redirected_body]
       end
     end
   end
@@ -62,6 +66,33 @@ describe Faraday::Encoding do
         response = client.get('/')
         expect(response.body.encoding).to eq(Encoding::ASCII_8BIT)
       end
+    end
+  end
+
+  context 'deal correctly with redirects that specify an encoding' do
+    # First request response with a redirect + a charset in the content-type header
+    let(:response_status) { 301 }
+    let(:response_headers) do
+      {
+        'content-type' => "text/plain; charset=utf-8",
+        'location' => '/redirected'
+      }
+    end
+    let(:response_body) { 'Redirected to /redirected' }
+
+    # The second request does not specify a charset in the content-type
+    let(:redirected_status) { 200 }
+    let(:redirected_headers) do
+      {
+        'content-type' => "text/html"
+      }
+    end
+    let(:redirected_body) do
+      'abc'.force_encoding(Encoding::ASCII_8BIT)
+    end
+    it 'does not memoize redirect encoding' do
+      response = client.get('/')
+      expect(response.body.encoding).to eq(Encoding::ASCII_8BIT)
     end
   end
 end
